303f0dfb5a7ab56442eaa83b4db6a15896ba1744
kishorearvind <kisharvind@gmail.com>
2013-04-01 19:21:58 +0530

    Initial commit for TTE

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tte::Application.config.secret_token = 'ade94955c3f0dd4e3ee4806370f04ef9825efbb28d80ddc8d5a998d5ac6b6ee0c67a80946b1e9c9c0eb594f7acbf724e3dbbaa41a0dd56e9e60315b7462b9382'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tte::Application.config.session_store :cookie_store, key: '_tte_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tte::Application.config.session_store :active_record_store
